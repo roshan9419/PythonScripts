95522065763f311bc76e080f7c8d1e72916786c9
Roshan Kumar <53408220+roshan9419@users.noreply.github.com>
2021-01-23 00:33:03 +0530

    Create googleImageDownloader.py

---
+++ googleImageDownloader.py
@@ -0,0 +1,25 @@
+import requests
+from bs4 import BeautifulSoup
+import urllib.request
+import os
+
+def downloadImage(query, fileLocation, n=4):
+	URL = "https://www.google.com/search?tbm=isch&q=" + query
+	result = requests.get(URL)
+	src = result.content
+
+	soup = BeautifulSoup(src, 'html.parser')
+	imgTags = soup.find_all('img', class_='t0fcAb')
+
+	count=0
+	for i in imgTags:
+		if count==n: break
+		try:
+			urllib.request.urlretrieve(i['src'], f'{fileLocation}/' + str(count) + '.jpg')
+			count+=1
+			print('Downloaded', count)
+		except Exception as e:
+			raise e
+      
+      
+downloadImage("India", C:\Users\Roshan\Desktop, 10) #10 images will be downloaded 
